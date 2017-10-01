9b72cef8627bfa80a7ec11005b760042c5a75cd2
Stuart Laughlin <stuart@bistrotech.net>
2017-10-01 17:30:30 -0500

    implements hamming distance computation for javascript

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -1,7 +1,25 @@
-// Define your Hamming class here.
-// `npm test` to ensure the unit tests pass and
-// your code meets all of the conditions.
-// You may use ES6 or ES5 to solve.
+function zip(strand1, strand2) {
+  const s1 = Array.from(strand1);
+  const s2 = Array.from(strand2);
+  return s1.map((el, idx) => [el, s2[idx]]);
+}
 
+class Hamming {
+  compute(strand1, strand2) {
+    let dist = 0;
+    if (strand1.length !== strand2.length) {
+      throw 'DNA strands must be of equal length.';
+    }
+
+    if (strand1 === strand2) {
+      return 0;
+    }
+
+    zip(strand1, strand2)
+      .forEach((pair) => { if (pair[0] !== pair[1]) dist += 1; });
+
+    return dist;
+  }
+}
 
 module.exports = Hamming;
